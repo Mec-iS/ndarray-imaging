7e75bc634aa1d2a39d012fa3a53472ebc35dc3c5
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2021-02-22 17:43:04 +0000

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ndarray-imaging
-image processing with numpy basics
+
+Image processing with numpy basics and [`scikit-image`](https://scikit-image.org/docs/dev/install.html#install-via-pip).
+
+Exercises from the book [Python 3 Image Processing](https://g.co/kgs/KmM7Jq).
+
+
---
+++ src/basics/__init__.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+from os.path import dirname, realpath, join
+
+assetsdir = join(dirname(dirname(dirname(realpath(__file__)))), "data", "aerials")
+
+def rgb2gray(img):
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+    return (
+        0.2989 * r + 0.5879 * g + 0.114 * b
+    )
